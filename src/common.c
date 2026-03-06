bd672082710174c3c9024db4b38edb84204a2d2f
Prashant Rahul <prashantrahul141@protonmail.com>
2026-03-06 10:33:36 +0530

    feat: add common with load_binary_file
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

---
+++ src/common.c
@@ -0,0 +1,31 @@
+#include "common.h"
+#include "kvm.h"
+#include "nhlog.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void load_binary_file(kvm *vm, const char *filepath) {
+  DEBUG("load_binary_file: %s", filepath);
+  int32_t fd = open(filepath, O_RDONLY);
+  if (fd < 0) {
+    FATAL("Opening binary file: %s", strerror(errno));
+    exit(-1);
+  }
+
+  uint8_t *p = (uint8_t *)vm->ram_start;
+  while (true) {
+    int32_t ret = read(fd, p, 4096);
+    if (ret <= 0) {
+      break;
+    }
+    TRACE("read_size = %d", ret);
+    p += ret;
+  }
+}
---
+++ src/common.h
@@ -0,0 +1,8 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "kvm.h"
+
+void load_binary_file(kvm *vm, const char *filepath);
+
+#endif // _COMMON_H_
